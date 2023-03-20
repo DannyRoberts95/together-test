3b79a8246d685deafa14ac36b99b4bf7c47509f7
dannyroberts95 <danielrdhr@gmail.com>
2023-03-20 10:00:35 +0000

    added assets

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -4,5 +4,10 @@ import { GlobalContext } from "~/utils/context";
 export default function Footer() {
 	// eslint-disable-next-line no-unused-vars
 	const [context] = useContext(GlobalContext);
-	return <div>{/* <pre>{JSON.stringify(context.options, null, 4)}</pre> */}</div>;
+	return (
+		<div>
+			Footer
+			{/* <pre>{JSON.stringify(context.options, null, 4)}</pre> */}
+		</div>
+	);
 }
--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -4,5 +4,10 @@ import { GlobalContext } from "~/utils/context";
 export default function Header() {
 	// eslint-disable-next-line no-unused-vars
 	const [context] = useContext(GlobalContext);
-	return <div>{/* <pre>{JSON.stringify(context.options, null, 4)}</pre> */}</div>;
+	return (
+		<div>
+			HEADER
+			{/* <pre>{JSON.stringify(context.options, null, 4)}</pre> */}
+		</div>
+	);
 }
