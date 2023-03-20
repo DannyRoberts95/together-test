3bb5e7290faf5d05c34c3d531f91260bda656a7e
dannyroberts95 <danielrdhr@gmail.com>
2023-03-20 17:22:29 +0000

    Partner Images

--- src/components/flexible/ImageCarousel.js
+++ src/components/flexible/ImageCarousel.js
@@ -1,44 +1,31 @@
 import Image from "next/image";
 import React, { useRef } from "react";
-import { motion, useScroll } from "framer-motion";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { wrap } from "@motionone/utils";
-
-function ParallaxChildren({ children, baseVelocity = 100 }) {
-	return (
-		<div className="relative flex overflow-x-hidden">
-			<div className="animate-marquee flex whitespace-nowrap py-12">
-				<span>{children}</span>
-				<span>{children}</span>
-				<span>{children}</span>
-				<span>{children}</span>
-			</div>
-
-			<div className="animate-marquee2 flex whitespace-nowrap py-12">
-				<span>{children}</span>
-				<span>{children}</span>
-				<span>{children}</span>
-				<span>{children}</span>
-			</div>
-		</div>
-	);
-}
+import { motion } from "framer-motion";
 
 function ImageCarousel(props) {
 	const { images = [], title = "" } = props;
+	const scrollRef = useRef(null);
 
 	// TODO ADD MOTION
 
 	const imgs = images.map((image, i) => (
-		<span className="mx-4 text-4xl">
-			<Image key={(image, i)} layout="intrinsic" loading="lazy" blurDataURL={image} src={image} alt="" height={50} width={110} objectFit="contain" />
-		</span>
+		<motion.span
+			className="mx-4 text-4xl"
+			initial={{ opacity: 0 }}
+			whileInView={{ opacity: 1 }}
+			viewport={{ root: scrollRef }}
+			transition={{ ease: "easeIn", duration: 0.5 * i, delay: 0.5 * i - 0.5 }}
+		>
+			<Image key={(image, i)} layout="intrinsic" blurDataURL={image} src={image} alt="" height={50} width={110} objectFit="contain" />
+		</motion.span>
 	));
 
 	return (
-		<div className="flex flex-col items-center  overflow-hidden">
+		<div className="flex flex-col items-center overflow-hidden">
 			{title && <p className="text-h4 my-8">{title}</p>}
-			<div className="relative mt-12 flex w-full">{imgs}</div>
+			<div ref={scrollRef} className="relative mt-12 flex w-full justify-between">
+				{imgs}
+			</div>
 		</div>
 	);
 }
